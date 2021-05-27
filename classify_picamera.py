d3d208d03e4a436bbf9f85158e8015687207c2b0
gchoi17 <gchoi17@illinois.edu>
2021-05-27 11:15:42 -0500

    added some comments

--- classify_picamera.py
+++ classify_picamera.py
@@ -13,7 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Example using TF Lite to classify objects with the Raspberry Pi camera."""
+# Kevin Choi
+# Modified the image classification example code to apply on our system
 
 from __future__ import absolute_import
 from __future__ import division
@@ -37,12 +38,12 @@ from os import path
 from PIL import Image
 from tflite_runtime.interpreter import Interpreter
 
-
+# load the object labels
 def load_labels(path):
   with open(path, 'r') as f:
     return {i: line.strip() for i, line in enumerate(f.readlines())}
 
-
+# set up the tensor with the input model
 def set_input_tensor(interpreter, image):
   tensor_index = interpreter.get_input_details()[0]['index']
   input_tensor = interpreter.tensor(tensor_index)()[0]
@@ -124,91 +125,96 @@ class Sheets_Logging:
            valueInputOption='USER_ENTERED', body=body).execute()
 
 def main():
-  doc = Sheets_Logging()
-  GPIO.setwarnings(False)
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(17, GPIO.OUT)
+    doc = Sheets_Logging()
+    # Set up the analog signal sent to the buzzer
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.OUT)
 
-  values = doc.read_data()
-  cur_time = datetime.now()
-  is_expired = 0
-  for i, val in enumerate(values):
-      if i:
-          exp_date = datetime.strptime(val[2], '%Y-%m-%d %H:%M:%S')
-          if exp_date < cur_time:
-              is_expired = 1
-              print(val) 
-              break
-  if is_expired:
-    GPIO.output(17, True)
-    time.sleep(5)
-    GPIO.output(17, False)
+    # Read the data, find any expired item and alert the user if there is
+    values = doc.read_data()
+    cur_time = datetime.now()
+    is_expired = 0
+    for i, val in enumerate(values):
+        if i:
+            exp_date = datetime.strptime(val[2], '%Y-%m-%d %H:%M:%S')
+            if exp_date < cur_time:
+                is_expired = 1
+                print(val) 
+                break
+    if is_expired:
+        GPIO.output(17, True)
+        time.sleep(5)
+        GPIO.output(17, False)
 
-  parser = argparse.ArgumentParser(
-      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument(
-      '--model', help='File path of .tflite file.', required=True)
-  parser.add_argument(
-      '--labels', help='File path of labels file.', required=True)
-  args = parser.parse_args()
+    # Load the model and the labels, set the corresponding expiration dates
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--model', help='File path of .tflite file.', required=True)
+    parser.add_argument(
+        '--labels', help='File path of labels file.', required=True)
+    args = parser.parse_args()
 
-  labels = load_labels(args.labels)
-  expiration_dates = {
-          "broccoli":       timedelta(days=5), 
-          "cauliflower":    timedelta(weeks=1), 
-          "zucchini":       timedelta(days=5), 
-          "cucumber":       timedelta(days=10), 
-          "artichoke":      timedelta(days=6), 
-          "bell pepper":    timedelta(weeks=1), 
-          "mushroom":       timedelta(weeks=1),
-          "Granny Smith":   timedelta(weeks=4),
-          "strawberry":     timedelta(days=5),
-          "orange":         timedelta(weeks=4),
-          "lemon":          timedelta(weeks=4),
-          "pineapple":      timedelta(days=4),
-          "banana":         timedelta(days=2),
-          "pomegranate":    timedelta(weeks=4),
-          "meat loaf":      timedelta(days=5),
-          "pizza":          timedelta(days=3),
-          "potpie":         timedelta(days=3),
-          "burrito":        timedelta(days=3),
-          "red wine":       timedelta(days=3)
-          }
-  interpreter = Interpreter(args.model)
-  interpreter.allocate_tensors()
-  _, height, width, _ = interpreter.get_input_details()[0]['shape']
+    labels = load_labels(args.labels)
+    expiration_dates = {
+            "broccoli":       timedelta(days=5), 
+            "cauliflower":    timedelta(weeks=1), 
+            "zucchini":       timedelta(days=5), 
+            "cucumber":       timedelta(days=10), 
+            "artichoke":      timedelta(days=6), 
+            "bell pepper":    timedelta(weeks=1), 
+            "mushroom":       timedelta(weeks=1),
+            "Granny Smith":   timedelta(weeks=4),
+            "strawberry":     timedelta(days=5),
+            "orange":         timedelta(weeks=4),
+            "lemon":          timedelta(weeks=4),
+            "pineapple":      timedelta(days=4),
+            "banana":         timedelta(days=2),
+            "pomegranate":    timedelta(weeks=4),
+            "meat loaf":      timedelta(days=5),
+            "pizza":          timedelta(days=3),
+            "potpie":         timedelta(days=3),
+            "burrito":        timedelta(days=3),
+            "red wine":       timedelta(days=3)
+            }
+    interpreter = Interpreter(args.model)
+    interpreter.allocate_tensors()
+    _, height, width, _ = interpreter.get_input_details()[0]['shape']
 
-  with picamera.PiCamera() as camera:
-    camera.resolution = (width, height)
-    camera.start_preview()
-    count = 0
-    prev_label_id = 0
-    while True:
-        stream = io.BytesIO()
-        try:
-            camera.capture(stream, format='jpeg')
-            image = Image.open(stream).convert('RGB')
-            results = classify_image(interpreter, image)
-            label_id, prob = results[0]
-            stream.seek(0)
-            stream.truncate()
-            print(labels[label_id], prob)
-            if prob > .8 and label_id == prev_label_id:
-                count += 1
-                if count > 4:
-                    if labels[label_id] in expiration_dates.keys():
-                        doc.write_data([str(datetime.now()).split('.')[0], labels[label_id], str(datetime.now()+expiration_dates[labels[label_id]]).split('.')[0]])
-                        GPIO.output(17, True)
-                        time.sleep(5)
-                        GPIO.output(17, False)
+    # Regonize the image from the cimera, validate and write to the databse
+    with picamera.PiCamera() as camera:
+        camera.resolution = (width, height)
+        camera.start_preview()
+        count = 0
+        prev_label_id = 0
+        while True:
+            stream = io.BytesIO()
+            try:
+                camera.capture(stream, format='jpeg')
+                image = Image.open(stream).convert('RGB')
+                results = classify_image(interpreter, image)
+                label_id, prob = results[0]
+                stream.seek(0)
+                stream.truncate()
+                print(labels[label_id], prob)
+                if prob > .8 and label_id == prev_label_id:
+                    count += 1
+                    if count > 4:
+                        if labels[label_id] in expiration_dates.keys():
+                            # Valid for 5 consecutive times with high accuracy, and is a food item
+                            doc.write_data([str(datetime.now()).split('.')[0], labels[label_id], str(datetime.now()+expiration_dates[labels[label_id]]).split('.')[0]])
+                            GPIO.output(17, True)
+                            time.sleep(5)
+                            GPIO.output(17, False)
+                        count = 0
+                        print(labels[label_id] in expiration_dates.keys())
+                elif label_id != prev_label_id:
                     count = 0
-                    print(labels[label_id] in expiration_dates.keys())
-            elif label_id != prev_label_id:
-                count = 0
-            prev_label_id = label_id
-            time.sleep(.2)
-        finally:
-            pass
+                prev_label_id = label_id
+                time.sleep(.2)
+            finally:
+                pass
 
 if __name__ == '__main__':
-  main()
+    main()
